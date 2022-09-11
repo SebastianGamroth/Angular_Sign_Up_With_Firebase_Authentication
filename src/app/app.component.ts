0d469253b8fe039a190f098ea10bf96eb9b88d35
user7d11 <user7w11@gmail.com>
2022-09-11 17:37:00 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular_Sign_Up_With_Firebase_Authentication';
+}
