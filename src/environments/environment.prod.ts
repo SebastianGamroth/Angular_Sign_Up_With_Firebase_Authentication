fe3fd3d6ccbd41dbe30b37a089a57341c2b22fd5
user7d11 <user7w11@gmail.com>
2022-09-12 19:16:44 +0200

    new projekt firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-sign-up-227d3',
+    appId: '1:637159179256:web:5ed437633eefc6adcf0c0d',
+    storageBucket: 'angular-sign-up-227d3.appspot.com',
+    apiKey: 'AIzaSyDf8L1HVKTJbgw2rE6mJ78hEsll_rwCEJE',
+    authDomain: 'angular-sign-up-227d3.firebaseapp.com',
+    messagingSenderId: '637159179256',
+  },
   production: true
 };
